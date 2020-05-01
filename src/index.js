c006707fbb54f395a9bfaa7e725b1bb6cfa88484
Andrew <andrewunifei@gmail.com>
2020-04-30 23:08:11 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bubbleSort = require('./bubbleSort')
+const app = express();
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    const { array } = req.body;
+
+    const sorted = bubbleSort(array);
+
+    return res.send({sorted});
+})
+
+app.listen(3333); 
